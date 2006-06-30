0b6c95744f5152815ecaa9bf66484c7aa00f6ed3
jonathan <jonathan@20afb1e0-9c0e-0410-9884-91ed27886737>
2006-06-30 05:10:34 +0000

    git-svn-id: http://svn.viney.net.nz/things/rails/plugins/acts_as_ordered@79 20afb1e0-9c0e-0410-9884-91ed27886737

--- lib/acts_as_ordered.rb
+++ lib/acts_as_ordered.rb
@@ -4,19 +4,18 @@ module ActiveRecord
       def self.included(base)
         base.extend(ClassMethods)
       end
-    end
-
-    module ClassMethods
-      def acts_as_ordered(options = {})
-        options.assert_valid_keys :order, :wrap, :condition, :scope
-        
-        options[:order]     = options[:order] ? "#{options[:order]}, #{primary_key}" : primary_key
-        options[:condition] = options[:condition].to_proc if options[:condition].is_a?(Symbol)
-        options[:scope]     = "#{options[:scope]}_id".to_sym if options[:scope].is_a?(Symbol) && options[:scope].to_s !~ /_id$/
-        options[:scope]   ||= '1 = 1'
-        
-        scope_condition_method = if options[:scope].is_a?(Symbol)
-          %(
+      
+      module ClassMethods
+        def acts_as_ordered(options = {})
+          options.assert_valid_keys :order, :wrap, :condition, :scope
+          
+          options[:order]     = options[:order] ? "#{options[:order]}, #{primary_key}" : primary_key
+          options[:condition] = options[:condition].to_proc if options[:condition].is_a?(Symbol)
+          options[:scope]     = "#{options[:scope]}_id".to_sym if options[:scope].is_a?(Symbol) && options[:scope].to_s !~ /_id$/
+          options[:scope]   ||= '1 = 1'
+          
+          scope_condition_method = if options[:scope].is_a?(Symbol)
+           %(
             def ordered_scope_condition
               if #{options[:scope].to_s}.nil?
                 "#{options[:scope].to_s} IS NULL"
@@ -24,87 +23,92 @@ module ActiveRecord
                 "#{options[:scope].to_s} = \#{#{options[:scope].to_s}}"
               end
             end
-          )
-        else
-          "def ordered_scope_condition() \"#{options[:scope]}\" end"
+           )
+          else
+            "def ordered_scope_condition() \"#{options[:scope]}\" end"
+          end
+          
+          class_eval <<-END
+            #{scope_condition_method}
+            
+            def adjacent_id(number)
+              ids = ordered_ids
+              ids.reverse! if number < 0
+              index = ids.index(self.id)
+              #{options[:wrap] ? 'ids[(index + number.abs) % ids.size]' : 'ids[index + number.abs] || ids.last'}
+            end
+            
+            def ordered_ids
+              connection.select_values("SELECT #{primary_key} FROM #{table_name} WHERE \#{ordered_scope_condition} ORDER BY #{options[:order]}").collect(&:to_i)
+            end
+          END
+          
+          cattr_accessor :_adjacent_condition
+          self._adjacent_condition = options[:condition]
+          
+          include InstanceMethods
+        end
+      end
+      
+      module InstanceMethods
+        def adjacent_record(number)
+          previous_record = self
+          loop do
+            adjacent_record = self.class.find(previous_record.adjacent_id(number))
+            matches = self.class._adjacent_condition ? self.class._adjacent_condition.call(adjacent_record) : true
+            
+            return adjacent_record if matches
+            return self if adjacent_record == self # If the search for a matching record failed
+            return self if previous_record == adjacent_record # If we've found the same adjacent_record twice
+            
+            previous_record = adjacent_record
+            number = number < 0 ? -1 : 1
+          end
         end
         
-        class_eval <<-END
-          cattr_accessor :_adjacent_condition
-          
-          #{scope_condition_method}
+        def current_total
+          self.class.count :conditions => ordered_scope_condition
+        end
         
-          def ordered_ids
-            connection.select_values("SELECT #{primary_key} FROM #{table_name} WHERE \#{ordered_scope_condition} ORDER BY #{options[:order]}").collect(&:to_i)
-          end
-          
-          def adjacent_id(number)
-            ids = ordered_ids
-            ids.reverse! if number < 0
-            index = ids.index(self.id)
-            #{options[:wrap] ? 'ids[(index + number.abs) % ids.size]' : 'ids[index + number.abs] || ids.last'}
-          end
-          
-          def adjacent_record(number)
-            record = self
-            loop do
-              adjacent_record = self.class.find(record.adjacent_id(number))
-              matches = self.class._adjacent_condition ? self.class._adjacent_condition.call(adjacent_record) : true
-              
-              return self if (!matches and ![record, adjacent_record].include?(self)) or adjacent_record == self
-              return adjacent_record if matches
-              
-              record = adjacent_record
-              number = number < 0 ? -1 : 1
-            end
-          end
-          
-          def current_total
-            self.class.count :conditions => ordered_scope_condition
-          end
-          
-          def current_index
-            ordered_ids.index(id)
-          end
-          
-          def current_position
-            current_index + 1
-          end
-          
-          def next(number = 1)
-            adjacent_record(number)
-          end
-          
-          def previous(number = 1)
-            adjacent_record(-number)
-          end
-          
-          def first_id
-            ordered_ids.first
-          end
-          
-          def last_id
-            ordered_ids.last
-          end
-          
-          def first
-            self.class.find(first_id)
-          end
-          
-          def last
-            self.class.find(last_id)
-          end
-          
-          def first?
-            id == first_id
-          end
-          
-          def last?
-            id == last_id
-          end
-        END
+        def current_index
+          ordered_ids.index(id)
+        end
         
-        self._adjacent_condition = options[:condition]
+        def current_position
+          current_index + 1
+        end
+        
+        def next(number = 1)
+          adjacent_record(number)
+        end
+        
+        def previous(number = 1)
+          adjacent_record(-number)
+        end
+        
+        def first_id
+          ordered_ids.first
+        end
+        
+        def last_id
+          ordered_ids.last
+        end
+        
+        def first
+          self.class.find(first_id)
+        end
+        
+        def last
+          self.class.find(last_id)
+        end
+        
+        def first?
+          id == first_id
+        end
+        
+        def last?
+          id == last_id
+        end
       end
     end
   end
